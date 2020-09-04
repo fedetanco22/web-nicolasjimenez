e922a25b5003806ee085e9085a7e34789201c71c
Federico Tanco <fedetanco84@gmail.com>
2020-09-04 19:15:18 -0300

    Carousel Marcas

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
-
+window.addEventListener('load', function () {
+    new Glider(document.querySelector('.carousel__lista'), {
+        slidesToShow: 5,
+        slidesToScroll: 5,
+        dots: '.carousel__indicadores',
+        arrows: {
+            prev: '.carousel__anterior',
+            next: '.carousel__siguiente'
+        }
+    });
+});
