704943439b509bea908a0314ca41750d746a1509
Federico Tanco <fedetanco84@gmail.com>
2020-09-04 22:04:03 -0300

    V0A

--- js/script.js
+++ js/script.js
@@ -1,11 +1,28 @@
 window.addEventListener('load', function () {
     new Glider(document.querySelector('.carousel__lista'), {
-        slidesToShow: 5,
-        slidesToScroll: 5,
-        dots: '.carousel__indicadores',
+        slidesToShow: 1,
+        slidesToScroll: 1,
+
         arrows: {
             prev: '.carousel__anterior',
             next: '.carousel__siguiente'
-        }
+        },
+        responsive: [
+            {
+                breakpoint: 567,
+                settings: { // Set to `auto` and provide item width to adjust to viewport
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    dots: '.carousel__indicadores'
+                }
+            }, {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 5,
+                    slidesToScroll: 5
+                }
+            }
+        ]
+
     });
 });
